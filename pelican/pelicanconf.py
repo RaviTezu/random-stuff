70fb0d6bc68e0675f7e9a8d1ee7a2c852759d0c8
Terminator <ravi-teja@live.com>
2014-02-25 01:10:12 +0530

    Add basic site

---
+++ pelican/pelicanconf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Ravi Teja'
+SITENAME = u'Blog | RaviTezu'
+SITEURL = ''
+THEME = 'theme'
+TIMEZONE = 'Asia/Kolkata'
+TAGLINE = "Systems Engineer, Python Programmer and a NoSQL fan. Enjoy Web programming, Scaling challenges and Cloud computing."
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Pelican', 'http://getpelican.com/'),
+          ('Python.org', 'http://python.org/'),
+          ('', 'http://jinja.pocoo.org/'),
+          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
